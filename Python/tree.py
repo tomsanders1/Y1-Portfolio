5efeda4a1c4d258ce8d12bfc5d4423e0a5068398
tomsanders1 <145353090+tomsanders1@users.noreply.github.com>
2023-10-09 13:46:23 +0100

    Add files via upload

---
+++ Python/tree.py
@@ -0,0 +1,25 @@
+answer1 = str(input("Is your character a civil engineer? (yes/no lowercase answers) "))
+if answer1 == "yes":
+    print("Your character is michael schofield!")
+else:
+    answer2 = str(input("Did your character kill the vice presidents brother? "))
+    if answer2 == "yes":
+        print("Your character is Lincoln Burrows!")
+    else:
+        answer3 = str(input("Is your character a mobster? "))
+        if answer3 == "yes":
+            print("Your character is Abruzzi")
+        else:
+            answer4 = str(input("Is your character a tennager? "))
+            if answer4 == "yes":
+                print("Your character is Tweener!")
+            else:
+                answer5 = str(input("Is your character evil?" ))
+                if answer5 == "yes":
+                    print("Your character is Tweener!")
+                else:
+                    answer6 = str(input("Is your character insane? "))
+                    if answer6 == "yes":
+                        print("Your character is Haywire!")
+                    else:
+                        print("You literally have no other options.")
